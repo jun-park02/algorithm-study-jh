ea2128dbdb1111a3c0517e999f9d604c12fbcb32
JHPark02 <applestrawberryg@gmail.com>
2023-09-06 16:55:15 +0900

    P_1929

---
+++ 2. Mathematics/P_1929_실버3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main(void) {
+	int M, N;
+	std::cin >> M >> N;
+
+	for (int i = M; i <= N; i++) {
+		int j = 2;
+		bool prime_number = true;
+
+		while (j <= sqrt(i)) {
+			if (i % j == 0) {
+				prime_number = false;
+				break;
+			}
+			j++;
+		}
+
+		if (prime_number && i > 1)
+			std::cout << i << "\n";
+	}
+
+	return 0;
+}
