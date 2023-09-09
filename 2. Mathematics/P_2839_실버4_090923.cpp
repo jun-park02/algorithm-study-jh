f4faf5374451c2be93746dded073cb45c1eabe7f
JHPark02 <applestrawberryg@gmail.com>
2023-09-09 18:46:36 +0900

    P_2839 Math 풀이

---
+++ 2. Mathematics/P_2839_실버4_090923.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main(void) {
+	int N; std::cin >> N;
+
+	int cnt = 0;
+
+	while (N > 0) {
+		if (N >= 5 && N % 5 == 0) {
+			std::cout << N / 5 + cnt;
+			return 0;
+		}
+
+		N -= 3;
+		cnt++;
+	}
+
+	N == 0 ? std::cout << cnt : std::cout << -1;
+
+	return 0;
+}
