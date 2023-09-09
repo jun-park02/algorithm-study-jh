a40046137e8742a5b957250813caa78c5cd5c44c
JHPark02 <applestrawberryg@gmail.com>
2023-09-09 18:38:19 +0900

    P_2839 DP 풀이

---
+++ 7. Dynamic-programming/P_2839_실버4_090823.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+
+int main(void) {
+	int N;
+	std::cin >> N;
+
+	int sugar[5001];
+	std::fill(sugar, sugar + 5001, 99999999);
+
+	sugar[3] = 1;
+	sugar[5] = 1;
+
+	for (int i = 6; i <= N; i++) 
+		sugar[i] = std::min(sugar[i - 3] + 1, sugar[i - 5] + 1);
+	
+	sugar[N] >= 99999999 ? std::cout << -1 : std::cout << sugar[N];
+	
+	return 0;
+}
