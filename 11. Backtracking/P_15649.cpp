8e236bb09f4ea712fb2009d992971b66bab93f21
junhyeok <applestrawberryg@gmail.com>
2025-01-09 11:27:16 +0900

    P_15649

--- 11. Backtracking/P_15649.cpp
+++ 11. Backtracking/P_15649.cpp
@@ -1,39 +1,41 @@
 #include <iostream>
 #include <vector>
 
+// 문제주소 : https://www.acmicpc.net/problem/15649
+// 메모리 및 시간 : 2020KB, 24ms
+
 int N, M;
 int number[9];
 
-void func(int cnt) {
-	if (cnt == M)
+void func(int cnt, std::vector<int> vec) {
+	if (cnt == M) {
+		 for (int i = 0; i < vec.size(); i++) {
+		 	std::cout << vec[i] << " ";
+		 }
+		 std::cout << "\n";
 		return;
+	}
 
 	for (int i = 1; i <= N; i++) {
 		if (number[i] == 0) {
-			std::cout << i << " ";
-
+			vec.push_back(i);
 			number[i] = 1;
-			func(cnt + 1);
+			func(cnt + 1, vec);
 			number[i] = 0;
+			vec.pop_back();
 		}
 	}
 }
 
 int main(void) {
-	// 1 <= M <= N <= 8 
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(0);
+
 	std::cin >> N >> M;
 
-	for (int i = 1; i <= N; i++) {
-		std::cout << i << " ";
-
-		int cnt = 1;
-
-		number[i] = 1;
-		func(cnt);
-		number[i] = 0;
-
-		std::cout << "\n";
-	}
+	int cnt = 0;
+	std::vector<int> vec;
+	func(cnt, vec);
 
 	return 0;
 }
